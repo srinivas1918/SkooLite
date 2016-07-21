33284aff9e2b5b61cd10242e70cebf1e2d612537
nalla srinivas <srinivas1918@gmail.com>
2016-07-21 12:01:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
 var mongoskin = require('mongoskin');
-var db = mongoskin.db('mongodb://127.0.0.1:27017/saima', {safe:true});
+var connection_string = '127.0.0.1:27017/saima';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +'saima'
+}
+var db = mongoskin.db(connection_string, {safe:true});
 var helper=mongoskin.helper;
 
 var dbOparations={db:db,helper:helper};
